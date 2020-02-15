4823e3d7b604b815ef3110b59af75aa069e1be0c
Dr. Andreas Fischer <45405681+AndreasFischer1985@users.noreply.github.com>
2020-02-15 09:18:20 +0100

    Add files via upload

--- k4cg.sensorscraper.py
+++ k4cg.sensorscraper.py
@@ -13,14 +13,16 @@ def tick():
     t1  = time.time() #set t1 to now (in seconds since epoch)
     if (t1 % 60 < 1) & (stopscraping==False): # start scraping once if time/60 is approximately 0
         print(time.ctime(t1) + " ; ") # ctime converts seconds to string
-        scrape(time.strftime('%Y-%m-%dT%H:%M:%SZ',time.gmtime(t1)))
+        scrape(time.strftime('%Y-%m-%dT%H:%M:%SZ',time.localtime(t1))) # use localtime for timestamps; gmtime converts seconds to UTC time_struct based on UTC
         stopscraping=True # don't start scraping again if you already did so this very second
     if t1 % 60 >= 1:
          stopscraping=False # set "stopscrapings" to False if the first second of the current minute is over
-    c1.config(text=time.strftime('%H:%M:%S',time.gmtime(t1))) # gmtime converts seconds to time_struct, strftime in turn formats string
+    c1.config(text=time.strftime('%H:%M:%S',time.localtime(t1))) 
     c1.after(200,tick) # trigger tick each 200 ms
 
 def scrape(timestamp):
+    door_status=requests.get("https://graphs.k4cg.org/api/datasources/proxy/1/query?db=sensors&q=SELECT last(\"value\") FROM \"door_status\"").json()["results"][0]["series"][0]["values"][0]
+    print("door_status:\t\t"+str(door_status))
     irc=requests.get("https://graphs.k4cg.org/api/datasources/proxy/1/query?db=sensors&q=SELECT last(\"value\") FROM \"irc\"").json()["results"][0]["series"][0]["values"][0]
     print("irc:\t\t\t"+str(irc))
     network_hosts=requests.get("https://graphs.k4cg.org/api/datasources/proxy/1/query?db=sensors&q=SELECT last(\"value\") FROM \"network_hosts\"").json()["results"][0]["series"][0]["values"][0]
@@ -39,7 +41,7 @@ def scrape(timestamp):
     print("darksky_ext_temp:\t"+str(darksky_ext_temp))
     openweathermap_ext_temp=requests.get("https://graphs.k4cg.org/api/datasources/proxy/1/query?db=sensors&q=SELECT last(\"value\") FROM \"openweathermap_ext_temp\"").json()["results"][0]["series"][0]["values"][0]
     print("openweathermap_ext_temp\t:"+str(openweathermap_ext_temp)+"\n\n")
-    p=pandas.DataFrame([l.cget("text"),timestamp,irc[1],irc[0],network_hosts[1],network_hosts[0],mqtt_consumer[1],mqtt_consumer[0],humidity[1],humidity[0],tinker_noise[1],tinker_noise[0],tinker_temp[1],tinker_temp[0],darksky_ext_temp[1],darksky_ext_temp[0],openweathermap_ext_temp[1],openweathermap_ext_temp[0]]).transpose()
+    p=pandas.DataFrame([l.cget("text"),timestamp,door_status[1],door_status[0],irc[1],irc[0],network_hosts[1],network_hosts[0],mqtt_consumer[1],mqtt_consumer[0],humidity[1],humidity[0],tinker_noise[1],tinker_noise[0],tinker_temp[1],tinker_temp[0],darksky_ext_temp[1],darksky_ext_temp[0],openweathermap_ext_temp[1],openweathermap_ext_temp[0]]).transpose()
     p.to_csv("./daten.csv", mode='a', header=False)
 
 def minus():
@@ -48,11 +50,14 @@ def minus():
 def plus():
     l.config(text=int(l.cget("text"))+1)
 
-p=pandas.DataFrame(["visitors","timestamp","irc","t1","network_hosts","t2","mqtt_consumer","t3","humidity","t4","tinker_noise","t5","tinker_temp","t6","darksky_ext_temp","t7","openweathermap_ext_temp","t8"]).transpose()
+p=pandas.DataFrame(["visitors","localtime","door_status","t0","irc","t1","network_hosts","t2","mqtt_consumer","t3","humidity","t4","tinker_noise","t5","tinker_temp","t6","darksky_ext_temp","t7","openweathermap_ext_temp","t8"]).transpose()
 p.to_csv("./daten.csv", mode='w', header=False)
 
+door_status=requests.get("https://graphs.k4cg.org/api/datasources/proxy/1/query?db=sensors&q=SELECT last(\"value\") FROM \"door_status\"").json()["results"][0]["series"][0]["values"][0]
 n=5
+if(door_status[1]==0): n=0
 if(len(sys.argv)>1): n=sys.argv[len(sys.argv)-1]
+   
 print ("Currently there are assumed to be %s lifeforms present in the K4CG.\n" % n)
        
 w=tkinter.Tk()
